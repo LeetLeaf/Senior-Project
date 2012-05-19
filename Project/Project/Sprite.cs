74ae43cbb7c3d1e996ed30c400d0f8cce654a484
jbube <jeffbube@gmail.com>
2012-05-19 00:11:25 -0400

    updated namespace

---
+++ Project/Project/Sprite.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace Project
+{
+    class Sprite
+    {
+        Texture2D textureImage;
+        protected Point frameSize;
+        Point currentFrame;
+        Point sheetSize;
+        int collisionOffset;
+        int timeSinceLastFrame=0;
+        int millisecondsPerFrame;
+        const int defaultMillisecondsPerFrame = 16;
+        protected Vector2 speed;
+        protected Vector2 postion;
+
+        public Sprite(Texture2D textureImage, Point frameSize, Point currentFrame, Point sheetSize,
+                      int collisionOffset, int millisecondsPerFrame, Vector2 speed, Vector2 postion)
+        {
+            this.textureImage = textureImage;
+            this.frameSize = frameSize;
+            this.currentFrame = currentFrame;
+            this.sheetSize = sheetSize;
+            this.collisionOffset = collisionOffset;
+            this.millisecondsPerFrame = millisecondsPerFrame;
+            this.speed = speed;
+            this.postion = postion;
+        }
+
+        public virtual void Update(GameTime gameTime, Rectangle clientBounds)
+        {
+            timeSinceLastFrame += gameTime.ElapsedGameTime.Milliseconds;
+            if (timeSinceLastFrame > millisecondsPerFrame)
+            {
+                timeSinceLastFrame = 0;
+                ++currentFrame.X;
+                if (currentFrame.X >= sheetSize.X)
+                {
+                    currentFrame.X = 0;
+                    ++currentFrame.Y;
+                    if (currentFrame.Y >= sheetSize.Y)
+                    {
+                        currentFrame.Y = 0;
+                    }
+                }
+            }
+        }
+        public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(textureImage,
+                postion,
+                new Rectangle(currentFrame.X * frameSize.X,
+                    currentFrame.Y * frameSize.Y,
+                    frameSize.X, frameSize.Y),
+                Color.White, 0, Vector2.Zero,
+                1f, SpriteEffects.None, 0);
+        }
+        public abstract Vector2 direction
+        {
+            get;
+        }
+
+        public Rectangle collisionRect
+        {
+            get
+            {
+                return new Rectangle
+                (
+                    (int)postion.X + collisionOffset,
+                    (int)postion.Y + collisionOffset,
+                    frameSize.X - (collisionOffset * 2),
+                    frameSize.Y - (collisionOffset * 2)
+                );
+            }
+        }
+    }
+}
