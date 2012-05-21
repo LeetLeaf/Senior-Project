7f08ca938df7ff27d6b451beae1d81f0a4fae01a
Kyle Keebler <kylekeebler@gmail.com>
2012-05-21 12:45:07 -0400

    Added walking animations (this needs more work)

--- Project/Project/Project/Player.cs
+++ Project/Project/Project/Player.cs
@@ -10,7 +10,7 @@ namespace com.Kyle.Keebler
     public class Player
     {
         //Properties
-        private Point frameSize = new Point(18, 32);
+        private Point frameSize = new Point(18, 24);
 
         public int FrameSizeX
         {
@@ -52,7 +52,7 @@ namespace com.Kyle.Keebler
         
         //Animation and Rendering Methods
         private int timeSinceLastFrame = 0;
-        private int millisecondsPerFrame = 120;
+        private int millisecondsPerFrame = 150;
 
 
         public Rectangle renderFrame()
@@ -61,21 +61,85 @@ namespace com.Kyle.Keebler
                     CurrentFrameY * FrameSizeY, FrameSizeX, FrameSizeY);
         }
 
-        private Point idleFrames = new Point(3,0);
+        //Idle Cycle South
+        private Point[] idleFramesSouth = new Point[2] {new Point(1,0),new Point (1,0)};
 
-        public void idleCycle(GameTime gameTime)
+        public void idleCycleSouth(GameTime gameTime)
         {
             timeSinceLastFrame += gameTime.ElapsedGameTime.Milliseconds;
             if (timeSinceLastFrame > millisecondsPerFrame)
             {
                 timeSinceLastFrame -= millisecondsPerFrame;
                 ++CurrentFrameX;
-                if (CurrentFrameX >= idleFrames.X)
+                if (CurrentFrameX >= idleFramesSouth[1].X)
                 {
-                    CurrentFrameX = 0;
+                    CurrentFrameX = idleFramesSouth[0].X;
                     ++CurrentFrameY;
-                    if (CurrentFrameY >= idleFrames.Y)
-                        CurrentFrameY = 0;
+                    if (CurrentFrameY >= idleFramesSouth[1].Y)
+                        CurrentFrameY = idleFramesSouth[0].Y;
+                }
+            }
+        }
+
+        //Walking Cycle South
+        private Point[] walkFramesSouth = new Point[2] { new Point(0, 0), new Point(3, 0) };
+
+        public void walkCycleSouth(GameTime gameTime)
+        {
+
+            timeSinceLastFrame += gameTime.ElapsedGameTime.Milliseconds;
+            if (timeSinceLastFrame > millisecondsPerFrame)
+            {
+                timeSinceLastFrame -= millisecondsPerFrame;
+                ++CurrentFrameX;
+                if (CurrentFrameX >= walkFramesSouth[1].X)
+                {
+                    CurrentFrameX = walkFramesSouth[0].X;
+                    ++CurrentFrameY;
+                    if (CurrentFrameY >= walkFramesSouth[1].Y)
+                        CurrentFrameY = walkFramesSouth[0].Y;
+                }
+            }
+        }
+
+        //Walking cycle West
+        private Point[] walkFramesWest = new Point[2] { new Point(0, 1), new Point(3, 1) };
+
+        public void walkCycleWest(GameTime gameTime)
+        {
+            CurrentFrameY = walkFramesWest[0].Y;
+            timeSinceLastFrame += gameTime.ElapsedGameTime.Milliseconds;
+            if (timeSinceLastFrame > millisecondsPerFrame)
+            {
+                timeSinceLastFrame -= millisecondsPerFrame;
+                ++CurrentFrameX;
+                if (CurrentFrameX >= walkFramesWest[1].X)
+                {
+                    CurrentFrameX = walkFramesWest[0].X;
+                    ++CurrentFrameY;
+                    if (CurrentFrameY >= walkFramesWest[1].Y)
+                        CurrentFrameY = walkFramesWest[0].Y;
+                }
+            }
+        }
+
+        //Walking Cycle East
+        private Point[] walkFramesEast = new Point[2] { new Point(0, 2), new Point(3, 2) };
+
+        public void walkCycleEast(GameTime gameTime)
+        {
+            CurrentFrameY = walkFramesEast[0].Y;
+            timeSinceLastFrame += gameTime.ElapsedGameTime.Milliseconds;
+            if (timeSinceLastFrame > millisecondsPerFrame)
+            {
+                timeSinceLastFrame -= millisecondsPerFrame;
+                ++CurrentFrameX;
+                if (CurrentFrameX >= walkFramesEast[1].X)
+                {
+                    CurrentFrameX = walkFramesEast[0].X;
+                    ++CurrentFrameY;
+                    if (CurrentFrameY >= walkFramesEast[1].Y)
+                        CurrentFrameY = walkFramesEast[0].Y;
                 }
             }
         }
