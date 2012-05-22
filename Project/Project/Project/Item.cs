0b430860db30a49f977bb5f9e4ee181160ba78af
jbube <jeffbube@gmail.com>
2012-05-21 22:01:06 -0400

    Added Item and Sword types.

---
+++ Project/Project/Project/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace com.Kyle.Keebler
+{
+    public class Item
+    {
+        public ItemType TypeOfItem { get; set; }
+        public int ExperiencePointValue { get; private set; }
+        public Vector2 Position { get; set; }
+    }
+}
---
+++ Project/Project/Project/ItemType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace com.Kyle.Keebler
+{
+    public enum ItemType
+    {
+        Treasure,
+        Armor,
+        Weapon,
+        Junk,
+        Corpse,
+        Landscape
+    }
+}
