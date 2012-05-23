e16916cbacafd1c39775232ab7b5ff4da10805d9
Kyle Keebler <kylekeebler@gmail.com>
2012-05-23 00:31:43 -0400

    Update and Draw methods added to Sword and Item

--- Project/Project/Project/Item.cs
+++ Project/Project/Project/Item.cs
@@ -5,6 +5,7 @@ using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
+
 namespace com.Kyle.Keebler
 {
     public class Item
@@ -15,5 +16,33 @@ namespace com.Kyle.Keebler
         public Vector2 Position { get; set; }
         public Texture2D Texture { get; set; }
 
+        public Item()
+        { }
+        public Item(String Name, Texture2D Texture, Vector2 Position, ItemType TypeOfItem)
+        {
+            this.Name = Name;
+            this.Texture = Texture;
+            this.Position = Position;
+            this.TypeOfItem = TypeOfItem;
+
+        }
+
+        public void Update(GameTime gameTime)
+        { 
+            
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, Position, Color.White);
+        }
+        public Rectangle CollisionRec
+        {
+            get
+            {
+                return new Rectangle(
+                    (int)Position.X + 2, (int)Position.Y + 2,
+                    Texture.Width - 2, Texture.Height - 2);
+            }
+        }
     }
 }
