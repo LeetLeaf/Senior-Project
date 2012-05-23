40ac32ca21f0d05a26c98ee3d79ef9bd3b6fdc7a
jbube <jeffbube@gmail.com>
2012-05-22 21:42:29 -0400

    Added some interfaces

---
+++ Project/Project/Project/IMoveable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace com.Kyle.Keebler
+{
+    interface IMoveable
+    {
+        void MovePosition(Direction direction, GameTime gameTime);
+
+    }
+}
---
+++ Project/Project/Project/IRenderable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace com.Kyle.Keebler
+{
+    interface IRenderable
+    {
+        void Initialize();
+        void Draw(SpriteBatch spriteBatch);
+        void Update(GameTime gameTime);
+    }
+}
