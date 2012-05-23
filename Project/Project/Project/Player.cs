ba8afdf57dd06a04ca5aac25cf011d73b90aa5a3
Kyle Keebler <kylekeebler@gmail.com>
2012-05-22 21:29:50 -0400

    Added move logic to Update

--- Project/Project/Project/Player.cs
+++ Project/Project/Project/Player.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace com.Kyle.Keebler
 {
@@ -18,6 +19,9 @@ namespace com.Kyle.Keebler
             Texture = texture;
             this.Position = Position;
             FrameSize = new Point(18, 24);
+            CollisionRec = new Rectangle(
+                (int)Position.X + 5, (int)Position.Y + 5,
+                FrameSize.X - 5, FrameSize.Y - 5);
         }
 
         public override void ChangeGraphic()
@@ -27,6 +31,33 @@ namespace com.Kyle.Keebler
         public override bool ResolveAttack()
         {
             throw new NotImplementedException();
+        }
+        public override void Update(GameTime gameTime)
+        {
+          
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                MovePosition(Direction.South, gameTime);
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                MovePosition(Direction.North, gameTime);
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                MovePosition(Direction.West, gameTime);
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                MovePosition(Direction.East, gameTime);
+            }
+            //Idle
+            if (Keyboard.GetState().IsKeyUp(Keys.Down) && Keyboard.GetState().IsKeyUp(Keys.Up)
+                && Keyboard.GetState().IsKeyUp(Keys.Right) && Keyboard.GetState().IsKeyUp(Keys.Left))
+            {
+                idleCycleSouth(gameTime);
+            }
+
         }
         public override void Draw(SpriteBatch spriteBatch)
         {
