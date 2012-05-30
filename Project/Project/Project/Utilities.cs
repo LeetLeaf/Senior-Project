5c5236eee24921e34704d64f0ac98c05945d071d
jbube <jeffbube@gmail.com>
2012-05-29 21:14:52 -0400

    added utility class with FlipDirection()

---
+++ Project/Project/Project/Utilities.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace com.Kyle.Keebler
+{
+    public class Utilities
+    {
+        public static Direction FlipDirection(Direction d)
+        {
+            switch (d)
+            {
+                case Direction.East:
+                    return Direction.West;
+                case Direction.North:
+                    return Direction.South;
+                case Direction.South:
+                    return Direction.North;
+                case Direction.West:
+                    return Direction.East;
+                default:
+                    throw new ArgumentException("Direction not supported");
+            }
+        }
+    }
+}
